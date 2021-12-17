493acef432fded00dfb79bf73a3cea9c8f976e20
Abdeljalil BENETTALEB <ibenettaleb@gmail.com>
2021-12-17 21:10:41 +0100

    Testing and refining explosions

--- Assets/Scripts/SequencePlayer.cs
+++ Assets/Scripts/SequencePlayer.cs
@@ -20,4 +20,18 @@ public class SequencePlayer : MonoBehaviour
 
         transform.GetChild(index).gameObject.SetActive(true);
     }
+
+    public void NextItem() {
+        if (transform.childCount <= 0) {
+            return;
+        }
+
+        index = Mathf.RoundToInt(Mathf.Repeat(index + 1, transform.childCount));
+
+        for (int i = 0; i < transform.childCount; i++) {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+
+        transform.GetChild(index).gameObject.SetActive(true);
+    }
 }
